9574926c2663f3f9a36799856d5b7cfe6864a1a2
unknown <m.zawadzki022@gmail.com>
2024-05-19 19:22:25 +0200

    projekt

--- src/data/offers.js
+++ src/data/offers.js
@@ -1,9 +1,9 @@
 const offers = [
   { name: "Oferta 1", isNew: true },
   { name: "Oferta 2", isNew: false },
-  { name: "Oferta 3", isNew: true },
+  { name: "Oferta 3", isNew: false },
   { name: "Oferta 4", isNew: false },
-  { name: "Oferta 5", isNew: true },
+  { name: "Oferta 5", isNew: false },
   { name: "Oferta 6", isNew: false },
 ];
 
